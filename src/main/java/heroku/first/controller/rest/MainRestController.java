fcba4a03f23a66f74053f0c9424f10c984e57f1b
Mahmud Salomov <mahmud.salomov.salomov@gmail.com>
2020-09-27 17:55:24 +0100

    a

--- src/main/java/heroku/first/controller/rest/MainRestController.java
+++ src/main/java/heroku/first/controller/rest/MainRestController.java
@@ -24,7 +24,7 @@ public class MainRestController {
     public ResponseEntity findPrimesAtkin(@RequestParam(name = "n") int n) throws InterruptedException {
 //        Thread.sleep(1000);
 
-        if (n<1000000000&&n>0)
+        if (n<1000000&&n>0)
         return ResponseEntity.ok(Number_Feature_Engine.isPrimesE(n));
         else return ResponseEntity.badRequest().body("Noto'g'ri son kiritilgan!");
     }
